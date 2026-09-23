743245fcc5cee80bc1d9fadfe1c81729b97a8072
Yarin_Ilan <yarinilan@gmail.com>
2026-09-23 15:22:43 +0300

    Update instruction memory to use new hex file

--- rtl/instruction_memory.sv
+++ rtl/instruction_memory.sv
@@ -7,7 +7,7 @@ module instruction_memory(
 logic [31:0] instruction_mem [0:255];
 
 initial begin
-    $readmemh("C:/Users/User/Desktop/project risc v/sim/instruction_data.hex", instruction_mem);
+    $readmemh("instruction_data_sum_arr.hex", instruction_mem);
 end
 
 assign instruction = instruction_mem[addr[9:2]]; // word aligned address + reduced to 8 bits for 256 instructions
