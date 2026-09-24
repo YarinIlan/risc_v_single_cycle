627f39bb1da570200fbb36cf2a18db9d19e4f0ff
Yarin_Ilan <yarinilan@gmail.com>
2026-09-24 14:50:31 +0300

    Add files via upload

---
+++ tb/risc_v_top_compliance_add_tb.sv
@@ -0,0 +1,33 @@
+module risc_v_top_compliance_add_tb;
+
+    logic clk;
+    logic rst;
+
+    int clk_cycles = 0; //clock cycle counter
+
+    risc_v_top dut(
+        .clk(clk),
+        .rst(rst)
+    );
+
+    always #5 clk = ~clk; //generate clock
+    always_ff@(posedge clk) begin
+        clk_cycles <= clk_cycles + 1;
+    end
+
+    initial begin
+        $dumpfile("sim/dump.vcd"); //create a dump file
+        $dumpvars(0, risc_v_top_compliance_add_tb);
+        $readmemh("sim/compliance_add.hex", dut.datapath_instance.instruction_memory_instance.instruction_mem);
+
+        clk = 1'b0; //initialize system
+        rst = 1'b1;
+        #15
+
+        rst = 1'b0;// let the system run
+        #350
+
+        $writememh("sim/signature.output", dut.datapath_instance.data_memory_instance.memory);
+    end
+
+endmodule
