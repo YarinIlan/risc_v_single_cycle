d3b24b98975a364c0492b9f52884f2b64c65a9b2
Yarin_Ilan <yarinilan@gmail.com>
2026-09-24 14:47:08 +0300

    Refactor instruction_memory module
    Removed initial block for reading memory and simplified module structure.

--- rtl/instruction_memory.sv
+++ rtl/instruction_memory.sv
@@ -6,10 +6,6 @@ module instruction_memory(
 
 logic [31:0] instruction_mem [0:255];
 
-initial begin
-    $readmemh("instruction_data_sum_arr.hex", instruction_mem);
-end
-
 assign instruction = instruction_mem[addr[9:2]]; // word aligned address + reduced to 8 bits for 256 instructions
 
 endmodule
